e8b11c495ed942d0ad01c28c5e67862611eb8a28
Ruslan <tsarenkoruslan@gmail.com>
2019-03-06 02:24:23 +0200

    slider finished

--- app/js/main.js
+++ app/js/main.js
@@ -14,38 +14,37 @@ var headMenuBtn = document.querySelector ('.main-header__btn-nav');
 
 	$(document).ready(function(){
 
-/*$('.section-quote__btn-slider-next').click(function(){
-	var currentImage = $('.quote-slider__item.curry');
-	var currentImageIndex = $('.quote-slider__item.curry').index();
-	var nextImageIndex = currentImageIndex + 1;
-	var nextImage = $('.quote-slider__item').eq(nextImageIndex);
-	currentImage.fadeOut(1500);
-	currentImage.removeClass('curry');
-
-	if(nextImageIndex == ($('.quote-slider__item:last').index()+1)){
-		$('.quote-slider__item').eq(0).fadeIn(1500);
-		$('.quote-slider__item').eq(0).addClass('curry');
-	} else {
-		nextImage.fadeIn(1500);
-		nextImage.addClass('curry');
-	}
-})
-
-$('.section-quote__btn-slider-prev').click(function(){
-var currentImage = $('.quote-slider__item.curry');
-	var currentImageIndex = $('.quote-slider__item.curry').index();
-	var prevImageIndex = currentImageIndex - 1;
-	var prevImage = $('.quote-slider__item').eq(prevImageIndex);
-
-currentImage.fadeOut(1500);
-currentImage.removeClass('curry');
-prevImage.fadeIn(1500);
-prevImage.addClass('curry');
-
-
-});*/
 $('.single-item').slick({
 	dots: true
+});
+
+$('.center').slick({
+dots: true,
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '6.25%',
+        slidesToShow: 1
+      }
+    }
+  ]
+
 });
 
 });
